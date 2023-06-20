fa76730af2a62b2b7be1d80a1344ca7326e23f52
Harshil Agrawal (harshil1712) <github@harshil.dev>
2023-06-20 12:10:29 +0200

    ✨ First commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+  import Card from './lib/Card.svelte'
+  import {hasVat} from './lib/store'
+  const data = [{
+    title: "Prodcut 1",
+    description: "This is a product you can buy only here",
+    price: 30,
+  },
+  {
+    title: "Prodcut 2",
+    description: "This is a product you can buy only here",
+    price: 60,
+  },
+  {
+    title: "Prodcut 3",
+    description: "This is a product you can buy only here",
+    price: 10,
+  }]
+</script>
+
+<main>
+  <h1>Vite + Svelte</h1>
+  <div>
+    {#each data as item}
+		<Card title={item.title} description={item.description} price={item.price} />
+	{/each}
+  </div>
+</main>
+
+<style global>
+  @import "https://unpkg.com/open-props";
+  @import "https://unpkg.com/open-props/normalize.min.css";
+
+</style>
---
+++ src/lib/Card.svelte
@@ -0,0 +1,28 @@
+<script>
+    import {hasVat} from './store'
+    export let title, description, price=0
+    let hasVatValue;
+    hasVat.subscribe(value => {
+        hasVatValue = value;
+    })
+</script>
+
+<div class="card">
+    <h2>{title}</h2>
+    <p>{description}</p>
+    <span>$ {hasVatValue ? price*1.2 : price}</span>
+</div>
+
+<style>
+    .card {
+        border: 1px solid tomato;
+        width: 500px;
+    }
+    h2 {
+        font-size: 30px;
+        font-weight: var(--font-weight-8);
+    }
+    p {
+        font-weight: var(--font-weight-4);
+    }
+</style>
